3433673cd9cc520a1b47ed71734932031999ff40
Sahil pillania <sahilpillania88@gmail.com>
2024-04-01 19:42:07 +0530

    homepage

--- lib/app.dart
+++ lib/app.dart
@@ -1,10 +1,18 @@
 import "package:flutter/material.dart";
 
-class name extends StatelessWidget {
-  const name({super.key});
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
 
+  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return MaterialApp(
+      title: 'Flutter Memer App',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      // home: const M(title: 'Flutter Demo Home Page'),
+    );
   }
 }
