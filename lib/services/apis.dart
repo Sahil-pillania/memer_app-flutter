e01b093f00f8cd036f529cc9cb9d4d3eb112683f
Sahil pillania <sahilpillania88@gmail.com>
2024-04-09 20:35:21 +0530

    updated the data return type

--- lib/services/apis.dart
+++ lib/services/apis.dart
@@ -6,13 +6,13 @@ import "package:memer_app/model/memes_response.dart";
 abstract class API {
   static const String _baseUrl = "https://api.imgflip.com";
 
-  static Future<MemeData> getMemes() async {
+  static Future<MemeResponse> getMemes() async {
     final response = await http.get(Uri.parse('$_baseUrl/get_memes'));
 
     if (response.statusCode == 200) {
       // parse the Json
       final MemeResponse memeResponse = MemeResponse.fromJson(jsonDecode(response.body));
-      return memeResponse.data!;
+      return memeResponse;
     } else {
       throw Exception("Failed to load memes");
     }
