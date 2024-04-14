f40d3389b57203513666a982f031cb54d876fde7
Sahil pillania <sahilpillania88@gmail.com>
2024-04-14 21:55:00 +0530

    Added image url open launcher

--- lib/views/home_view.dart
+++ lib/views/home_view.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
 import 'package:memer_app/services/apis.dart';
 import 'package:cached_network_image/cached_network_image.dart';
+import 'package:url_launcher/url_launcher.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class HomeView extends StatelessWidget {
   const HomeView({super.key});
@@ -17,56 +19,51 @@ class HomeView extends StatelessWidget {
           builder: (context, snapshot) {
             if (snapshot.hasData) {
               return GridView.custom(
-                // itemCount: snapshot.data!.data!.memes!.length,
-                // itemBuilder: (context, index) {
-                //   final meme = snapshot.data!.data!.memes![index];
-
-                //   return Card(
-                //       child: Column(
-                //     children: [
-                //       CachedNetworkImage(
-                //         imageUrl: meme.url!,
-                //         fit: BoxFit.contain,
-                //         placeholder: (context, url) => const SizedBox.square(
-                //             dimension: 20.0,
-                //             child: const CircularProgressIndicator()),
-                //         errorWidget: (context, url, error) =>
-                //             const Icon(Icons.error),
-                //       ),
-                //     ],
-                //   ));
-                // },
-                gridDelegate: SliverWovenGridDelegate.count(
-                  crossAxisCount: 2,
-                  mainAxisSpacing: 8,
-                  crossAxisSpacing: 8,
+                gridDelegate: SliverQuiltedGridDelegate(
+                  crossAxisCount: 4,
+                  mainAxisSpacing: 4,
+                  crossAxisSpacing: 4,
+                  repeatPattern: QuiltedGridRepeatPattern.inverted,
                   pattern: [
-                    WovenGridTile(1),
-                    WovenGridTile(
-                      5 / 7,
-                      crossAxisRatio: 0.9,
-                      alignment: AlignmentDirectional.centerEnd,
-                    ),
+                    QuiltedGridTile(2, 2),
+                    QuiltedGridTile(1, 1),
+                    QuiltedGridTile(1, 1),
+                    QuiltedGridTile(1, 2),
                   ],
                 ),
                 childrenDelegate: SliverChildBuilderDelegate(
                   (context, index) {
                     final meme = snapshot.data!.data!.memes![index];
 
-                    return Card(
-                        child: Column(
-                      children: [
-                        CachedNetworkImage(
-                          imageUrl: meme.url!,
-                          fit: BoxFit.contain,
-                          placeholder: (context, url) => const SizedBox.square(
-                              dimension: 20.0,
-                              child: const CircularProgressIndicator()),
-                          errorWidget: (context, url, error) =>
-                              const Icon(Icons.error),
+                    return InkWell(
+                      onTap: () => launchUrl(Uri.parse(meme.url!)),
+                      child: Stack(children: [
+                        Card(
+                          child: CachedNetworkImage(
+                            imageUrl: meme.url!,
+                            fit: BoxFit.contain,
+                            placeholder: (context, url) =>
+                                const SizedBox.square(
+                                    dimension: 20.0,
+                                    child: const CircularProgressIndicator()),
+                            errorWidget: (context, url, error) =>
+                                const Icon(Icons.error),
+                          ),
                         ),
-                      ],
-                    ));
+                        Positioned(
+                          bottom: 0,
+                          right: 0,
+                          left: 0,
+                          child: Container(
+                            padding: const EdgeInsets.all(8),
+                            color: Colors.black.withOpacity(0.5),
+                            child: Text(meme.name!,
+                                style: TextStyle(
+                                    color: Colors.white, fontSize: 12)),
+                          ),
+                        ),
+                      ]),
+                    );
                   },
                 ),
               );
